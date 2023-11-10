59aeb0e2d9e4a50b308f443510640e9982258499
Daniiljv <k.daniil2023@gmail.com>
2023-11-10 23:32:11 +0600

    Добавил новый класс для проверки

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Daniil1 daniil = new Daniil1("Daniil", 18);
+
+        System.out.println("Name of student - " + daniil.getName() +
+                "\nAge of student - " + daniil.getAge());
+    }
+}
